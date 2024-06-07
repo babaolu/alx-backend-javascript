4ad0ed1a1472be4b97ca211a0c8a5057157d9d87
Babatunde Adeoti <babaolu98@gmail.com>
2024-06-07 16:09:36 +0100

    Handling multiple promises

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.then((data) => {
+  return promise.then(() => {
     console.log('Got a response from the API');
     return { status: 200, body: 'success' };
-  }, (error) => Error(''));
+  }, () => Error(''));
 }
---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,8 @@
+import { uploadPhoto, createUser } from "./utils.js";
+
+
+export default async function handleProfileSignup() {
+  const photoName = await uploadPhoto().then((data) => data.body);
+  const userName = await createUser().then(data => Object.values(data).join(' '));
+  console.log(photoName, userName);
+}
