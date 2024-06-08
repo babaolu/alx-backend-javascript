0d96c286a4a876fb70e767f932b35e29894fb06e
Babatunde Adeoti <babaolu98@gmail.com>
2024-06-08 22:37:15 +0100

    Satisfying lint indentation rules

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -2,7 +2,7 @@ import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   const profile = await Promise.all([uploadPhoto(), createUser()])
-  .then(([photo, user]) => ({ photo, user }))
-  .catch(() => ({ photo: null, user: null }));
+    .then(([photo, user]) => ({ photo, user }))
+    .catch(() => ({ photo: null, user: null }));
   return profile;
 }
