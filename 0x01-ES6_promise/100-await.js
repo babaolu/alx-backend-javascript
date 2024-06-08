d11410a7a75b6728b893fac5c53a7e837148640a
Babatunde Adeoti <babaolu98@gmail.com>
2024-06-08 22:33:41 +0100

    Using Async/await

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,8 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  const profile = await Promise.all([uploadPhoto(), createUser()])
+  .then(([photo, user]) => ({ photo, user }))
+  .catch(() => ({ photo: null, user: null }));
+  return profile;
+}
