ccc3416fa4f12b82b41ea5f0d6f2e8edbdedb897
Babatunde Adeoti <babaolu98@gmail.com>
2024-08-07 17:14:52 +0100

    Basics of NodeJS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,27 @@
+const fs = require('node:fs');
+const { fileURLToPath } = require('node:url');
+
+function countStudents(path) {
+  fs.readFile(path, 'utf-8', (err, data) => {
+    if (err) throw new Error('Cannot load the database');
+
+    let lines = data.split('\n');
+    lines = lines.toSpliced(0, 1);
+    lines = lines.filter((line) => line);
+    
+    console.log('Number of students:', lines.length);
+    
+    for (let i = 0; i < lines.length; i++) {
+      lines[i] = lines[i].split(',');
+    }
+    const fields = new Set(lines.map(line => line[3]));
+    for (const field of fields) {
+      let fiLines = lines.filter(line => line[3] == field);
+      console.log(`Number of students in ${field}: ${fiLines.length}. List:`,
+        fiLines.reduce((acc, curVal) => acc.concat(', ', curVal[0]), '')
+          .slice(2));
+    }
+  });
+}
+
+module.exports = countStudents;
